9a93ef6d085252420bfc64ceca3ed21f64c4f76e
Coen ten Thije Boonkkamp <coen@coenttb.com>
2025-11-12 16:01:15 +0100

    chore: use semantic versioning for dependencies (0.1.0)

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/swift-standards/swift-rfc-2045", branch: "main"),
-        .package(url: "https://github.com/swift-standards/swift-rfc-2046", branch: "main")
+        .package(url: "https://github.com/swift-standards/swift-rfc-2045", from: "0.1.0"),
+        .package(url: "https://github.com/swift-standards/swift-rfc-2046", from: "0.1.0")
     ],
     targets: [
         .target(
