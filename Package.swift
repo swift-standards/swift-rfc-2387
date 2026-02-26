66a7177b474f3a99329e172ee4737185158cc258
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-02-26 10:27:03 +0100

    Flatten test structure and migrate to Apple Swift Testing

--- Package.swift
+++ Package.swift
@@ -31,7 +31,13 @@ let package = Package(
                 .product(name: "RFC 2046", package: "swift-rfc-2046"),
                 .product(name: "RFC 5322", package: "swift-rfc-5322")
     ]
-        )
+        ),
+        .testTarget(
+            name: "RFC 2387 Tests",
+            dependencies: [
+                "RFC 2387",
+            ]
+        ),
     ],
     swiftLanguageModes: [.v6]
 )
@@ -47,6 +53,7 @@ for target in package.targets where ![.system, .binary, .plugin, .macro].contain
         .enableUpcomingFeature("ExistentialAny"),
         .enableUpcomingFeature("InternalImportsByDefault"),
         .enableUpcomingFeature("MemberImportVisibility"),
+        .enableUpcomingFeature("NonisolatedNonsendingByDefault"),
         .enableExperimentalFeature("Lifetimes"),
         .enableExperimentalFeature("SuppressedAssociatedTypes"),
         .enableExperimentalFeature("SuppressedAssociatedTypesWithDefaults"),
